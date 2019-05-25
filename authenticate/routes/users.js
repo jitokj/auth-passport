e0d1c778c5cbcb8c2c53c8b067e9552f295b7969
jito <jitokj3@gmail.com>
2019-05-25 23:38:53 +0530

    validation of register form

--- authenticate/routes/users.js
+++ authenticate/routes/users.js
@@ -12,6 +12,39 @@ res.render('register');
 router.get('/login',(req,res)=>{
     res.render('login');
     });
+
+//submitting register form
+
+router.post('/register',(req,res)=>{
+        var name = req.body.name;
+        var username = req.body.username;
+        var email = req.body.email;
+        var password = req.body.password;
+        var password2= req.body.password2;
+ 
+        //validation
+        req.checkBody('name','Name is required').notEmpty();
+        req.checkBody('email','E-mail is required').notEmpty();
+        req.checkBody('email','E-mail is not valid').isEmail();
+        req.checkBody('username','UserName is required').notEmpty();
+        req.checkBody('password','password is required').notEmpty();
+        req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+        var errors = req.validationErrors();
+
+        if(errors){
+               
+            res.render('register',{
+            errors: errors
+            });
+        }
+        else
+        {
+          console.log('successfull');
+        }
+
+
+        });   
     
 
 
